f8fc7190cbbc498c0ae38c0bedf096974232225f
Kenny Su <kfernandosuqiu@gmail.com>
2021-10-20 17:20:47 -0400

    moved files

--- src/View.java
+++ src/View.java
@@ -5,6 +5,10 @@ public class View extends JFrame{
     public static final Color VERY_DARK_GREEN = new Color(0, 102, 0);
     public void initializeGame() {
         JFrame frame = new JFrame("Klondike Solitaire");
+        ImageIcon image;
+        image = new ImageIcon(getClass().getResource("/cards/two_of_spades.png"));
+        JLabel displayCard = new JLabel(image);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(2000,1000);
 
@@ -14,9 +18,11 @@ public class View extends JFrame{
         panel.add(newGame); // Components Added using Flow Layout
         panel.add(undo);
 
+
         // Text Area at the Center
         frame.getContentPane().setBackground(VERY_DARK_GREEN);
         frame.getContentPane().add(BorderLayout.NORTH, panel);
+        frame.add(displayCard);
         frame.setVisible(true);
 
         // Brian added a comment
